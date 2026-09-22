55b6643c8569618ab7f9620b267833de69c73c1b
Antonio-Moraaaaa <OdeliaIsabelle8484@outlook.com>
2026-09-22 10:59:37 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,14 +1,16 @@
-from fastapi import FastAPI
 from mcp.server.fastmcp import FastMCP
 
-mcp = FastMCP("Simple Text MCP HTTP")
+mcp = FastMCP(
+    "Simple Text MCP HTTP",
+    stateless_http=True
+)
 
 
 @mcp.tool()
 def echo_text(text: str) -> dict:
-    return {"text": text}
+    return {
+        "text": text
+    }
 
 
-app = FastAPI()
-
-app.mount("/", mcp.streamable_http_app())
+app = mcp.streamable_http_app()
